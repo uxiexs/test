70c5f561c047e451714c0ef6fe31f98ef3cee8c3
uxeix <uxeixs@aliyun.com>
2016-04-02 15:34:28 +0800

    button(普通按钮)

---
+++ php/Time.class.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: uxeix
+ * Date: 2016/4/2
+ * Time: 15:23
+ */
+
+namespace test\php;
+
+/**
+ * Class Time
+ * @package test\php
+ * 获取当前时间
+ */
+class Time
+{
+    public function __construct()
+    {
+        $this->_getTime();
+    }
+
+    //获取时间
+    protected function _getTime(){
+        $t = date('Y-m-d H:i:s',time());
+        echo '当前系统时间为：'.$t;
+    }
+}
+new Time();

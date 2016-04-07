3bd15d1cf0501735cd66b1a8f14a4e31a6771cb6
uxeix <uxeixs@aliyun.com>
2016-04-07 11:40:42 +0800

    字符串转换成int float

--- php/StrToInt.class.php
+++ php/StrToInt.class.php
@@ -46,4 +46,33 @@ var_dump($obj->action);
 <form action="" method="post">
     <input type="text" name="action">
     <input type="submit" value="提交">
+    <ul>
+        <li><em>代码：</em>
+        <textarea cols="60" rows="22">
+class StrToInt
+{
+    public $action;
+
+    /**
+     * StrToInt constructor. 构造函数
+     * @param $action 外部传参 执行类方法
+     */
+    public function __construct($action)
+    {
+        $this->action = $action;
+        $this->_run();
+    }
+
+    /**
+     * 功能：字符串转换成整型或浮点型
+     * @return int float
+     */
+    protected  function _run(){
+       return $this->action = $this->action + 0;
+    }
+}
+        </textarea>
+        </li>
+        <li><em>string转int float，只需要+0即可</em></li>
+    </ul>
 </form>

5d26932a24a5742bf9e6277acc3a2cd32214523b
Uxeix <uxiex@aliyun.com>
2016-05-12 09:41:28 +0800

    使用getScript()方法异步加载并执行js文件
    使用getScript()方法异步请求并执行服务器中的JavaScript格式的文件，它的调用格式如下所示：
    
    jQuery.getScript(url,[callback])或$.getScript(url,[callback])
    
    参数url为服务器请求地址，可选项callback参数为请求成功后执行的回调函数。

---
+++ JQuery/IMOOC/8-3/data/sport_f.js
@@ -0,0 +1,15 @@
+﻿var data = [{
+    "name": "足球"
+}, {
+    "name": "散步"
+}, {
+    "name": "篮球"
+}, {
+    "name": "乒乓球"
+}, {
+    "name": "骑自行车"
+}];
+$.each(data, function (index, sport) {
+    if (index == 1)
+        $("ul").append("<li>" + sport["name"] + "</li>");
+});

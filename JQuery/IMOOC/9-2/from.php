dc03b58b06f9da3828126b27a3e638117380778d
Uxeix <uxiex@aliyun.com>
2016-05-14 10:41:59 +0800

    表单插件——form
    通过表单form插件，调用ajaxForm()方法，实现ajax方式向服务器提交表单数据，并通过方法中的options对象获取服务器返回数据，调用格式如下：
    
    $(form). ajaxForm ({options})
    
    其中form参数表示表单元素名称；options是一个配置对象，用于在发送ajax请求过程，设置发送时的数据和参数。

---
+++ JQuery/IMOOC/9-2/from.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: uxeix
+ * Date: 2016/5/13
+ * Time: 16:57
+ */
+$user = $_POST["user"];
+$nick = $_POST["nick"];
+echo '用户名：'.$user."<br />".'昵称：'.$nick;
+?>
